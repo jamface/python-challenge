88eef576055ae966044e266ffd2baa562592fe00
Jamface <jlamb.8153@gmail.com>
2016-12-26 16:12:42 +0000

    Create dir run func function created and tested

--- lib/functions.py
+++ lib/functions.py
@@ -1,5 +1,6 @@
 from urllib import request
 import contextlib
+import os
 
 
 def get_url_resource(url, decoding='utf-8'):
@@ -16,6 +17,7 @@ def get_url_resource(url, decoding='utf-8'):
         result = response.read()
         return result if not decoding else result.decode(decoding)
 
+
 def get_last_number(resource):
     """
     Extract the last number from a resource.
@@ -28,3 +30,19 @@ def get_last_number(resource):
     numbers = [int(s) for s in resource.split() if s.isdigit()]
     if len(numbers) > 0:
         return numbers[-1]
+
+
+def create_dir_run_func(directory, func=None):
+    """
+    Create a directory if it does not exist and execute a
+    function on that directory if one is provided.
+
+    Args:
+        directory: the target directory to create and run a function on.
+        func: the optional function that should take the directory
+            path as the first argument.
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    if func and not os.listdir(directory):
+        func(directory)
--- lib/tests/functions_test.py
+++ lib/tests/functions_test.py
@@ -1,5 +1,7 @@
-from lib.functions import get_url_resource, get_last_number
+from lib.functions import get_url_resource, get_last_number, create_dir_run_func
 import unittest
+import shutil
+import os
 
 
 class GetUrlResourceTest(unittest.TestCase):
@@ -24,6 +26,29 @@ class GetLastNumberTest(unittest.TestCase):
         sample = ('There maybe misleading numbers in the text. One example '
                   'is 82683. Look only for the next nothing and the next nothing is 63579')
         self.assertEqual(63579, get_last_number(sample))
+
+
+class CreateDirRunFuncTest(unittest.TestCase):
+
+    TARGET = '../../out/test'
+
+    def setUp(self):
+        if os.path.exists(self.TARGET):
+            shutil.rmtree(self.TARGET)
+
+    def tearDown(self):
+        shutil.rmtree(self.TARGET)
+
+    def test_dirCreated(self):
+        create_dir_run_func(self.TARGET)
+        self.assertTrue(os.path.exists(self.TARGET))
+
+    def test_emptyDirRunFunction(self):
+        def touch(directory):
+            file = '{}/test.txt'.format(directory)
+            open(file, 'a').close()
+        create_dir_run_func(self.TARGET, func=touch)
+        self.assertTrue(os.listdir(self.TARGET))
 
 
 if __name__ == '__main__':
