6e34859611700fe0adb7e0a8d4e189063b086cc4
Jamface <jlamb.8153@gmail.com>
2016-12-26 17:40:14 +0000

    Travis CI test

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(name='python-challenge',
       version='1.0',
       author='Jamface',
       url='https://github.com/jamface/python-challenge',
-      py_modules=['utils/classes', 'utils/functions'])
+      packages=['utils'])
